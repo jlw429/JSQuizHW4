ff380fc56f9bba506302acd28dedf97fe7b11a4b
jlw429 <jlw00329@gmail.com>
2020-12-23 15:47:46 -0500

    rough javascript

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,30 @@
+const question = document.getElementById('question');
+let choices = document.getElementsByClassName('choice');
+console.log(choices);
+let count = 60;
+let score = 0;
+let scoreText = document.getElementById('Score');
+const score_points = 100;
+const max_questions = 5;
+let questionCounter = 0;
 
-
-
-const questions =[
+const questions = [
   {
     question: 'What can be used to store multiple values in a single variable?',
-    a: 'For Loop',
-    b: 'Object',
-    c: 'Array',
-    d: 'While Loop',
-    answer: 3,
+    answerA: 'For Loop',
+    answerB: 'Object',
+    answerC: 'Array',
+    answerD: 'While Loop',
+    correct: 3,
   },
   {
-    question: 'This type of loop executes a block of code as long as a specified question is TRUE',
-    a: 'For Loop',
-    b: 'While Loop',
-    c: 'if statement',
-    d: 'None of the above',
-    answer: 2,
+    question:
+      'This type of loop executes a block of code as long as a specified question is TRUE',
+    answerA: 'For Loop',
+    answerB: 'While Loop',
+    answerC: 'if statement',
+    answerD: 'None of the above',
+    correct: 2,
   },
   {
     question: 'This method rounds a number downward to its nearest integer',
@@ -24,7 +32,7 @@ const questions =[
     b: 'Math Ceiling',
     c: 'Math Trunc',
     d: 'Math Floor',
-    answer: 4,
+    correct: 4,
   },
   {
     question: 'This is a container for storing data values',
@@ -32,17 +40,36 @@ const questions =[
     b: 'sting',
     c: 'array',
     d: 'Integer',
-    answer: 1,
+    correct: 1,
   },
   {
-  question: 'What determines the accessibility or visibility of variables?',
+    question: 'What determines the accessibility or visibility of variables?',
     a: 'Scope',
     b: 'Object',
     c: 'variable',
     d: 'Javascript',
-    answer: 1,
+    correct: 1,
   },
 ];
 
-
+function startGame() {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...question];
+  newQuestion();
 }
+
+function newQuestion() {}
+
+//Timer
+
+setInterval(function () {
+  count--;
+  if (count >= 0) {
+    id = document.getElementById('countdown');
+    id.innerHTML = count;
+  }
+  if (count === 0) {
+    id.innerHTML = "Time's Up";
+  }
+}, 200);
