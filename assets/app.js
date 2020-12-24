99fdc03f0512fa911ff6a5c2e0d96fcae3ecbb2a
jlw429 <jlw00329@gmail.com>
2020-12-24 01:02:14 -0500

    working quiz! Still need to fix scores :(

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,8 @@
 const question = document.getElementById('question');
-let choices = document.getElementsByClassName('.choice-text');
+let choices = Array.from(document.querySelectorAll('.choice-text'));
 let count = 60;
 let score = 0;
-let scoreText = document.getElementById('Score');
-const score_points = 100;
+const score_points = 20;
 let acceptingAnswers = true;
 const max_questions = 5;
 let questionCounter = 0;
@@ -13,42 +12,42 @@ let availableQuestions = []
 let questions = [
   {
     question: 'What can be used to store multiple values in a single variable?',
-    choiceA: 'For Loop',
-    choiceB: 'Object',
-    choiceC: 'Array',
-    choiceD: 'While Loop',
+    choice1: 'For Loop',
+    choice2: 'Object',
+    choice3: 'Array',
+    choice4: 'While Loop',
     correct: 3,
   },
   {
     question: 'This type of loop executes a block of code as long as a specified question is TRUE',
-    choiceA: 'For Loop',
-    choiceB: 'While Loop',
-    choiceC: 'if statement',
-    choiceD: 'None of the above',
+    choice1: 'For Loop',
+    choice2: 'While Loop',
+    choice3: 'if statement',
+    choice4: 'None of the above',
     correct: 2,
   },
   {
     question: 'This method rounds a number downward to its nearest integer',
-    choiceA: 'Math Random',
-    choiceB: 'Math Ceiling',
-    choiceC: 'Math Trunc',
-    choiceD: 'Math Floor',
+    choice1: 'Math Random',
+    choice2: 'Math Ceiling',
+    choice3: 'Math Trunc',
+    choice4: 'Math Floor',
     correct: 4,
   },
   {
     question: 'This is a container for storing data values',
-    choiceA: 'variable',
-    choiceB: 'sting',
-    choiceC: 'array',
-    choiceD: 'Integer',
+    choice1: 'variable',
+    choice2: 'sting',
+    choice3: 'array',
+    choice4: 'Integer',
     correct: 1,
   },
   {
     question: 'What determines the accessibility or visibility of variables?',
-    a: 'Scope',
-    b: 'Object',
-    c: 'variable',
-    d: 'Javascript',
+    choice1: 'Scope',
+    choice2: 'Object',
+    choice3: 'variable',
+    choice4: 'Javascript',
     correct: 1,
   }
 ];
@@ -56,9 +55,9 @@ let questions = [
 function startGame() {
   questionCounter = 0;
   score = 0;
-  availableQuestions = [...question];
+  availableQuestions = [...questions];
   newQuestion();
-}
+};
 
 function newQuestion() {
   if(availableQuestions.length === 0 || questionCounter > max_questions) {
@@ -67,22 +66,21 @@ function newQuestion() {
     return window.location.assign('/end.html')
   }
     questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${max_questions}`;
-
-    let questionsIndex = Math.floor(math.random() * availableQuestions.length);
+    
+    let questionsIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
 
-    choices.forEach(function (choice) {
-        let num = choice.dataset['number'];
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
-    })
+    });
 
     availableQuestions.splice(questionsIndex, 1);
 
     acceptingAnswers = true
 
-  choices.forEach(function (choice) {
+    choices.forEach(function (choice) {
     choice.addEventListener('click', event => {
       if(!acceptingAnswers) return
 
@@ -98,24 +96,14 @@ function newQuestion() {
 
       selectedChoice.parentElement.classList.add(classToApply);
 
-      setTimeout(function () {
-        selectedChoice.parentElement.classList.remove(classToApply);
-        newQuestion();
       
-
-    }, 1000);
-
-
+        newQuestion();
+    
+    });
   });
-  
 };
 
-
-
-
-
 //Timer
-
 setInterval(function () {
   count--;
   if (count >= 0) {
@@ -125,4 +113,11 @@ setInterval(function () {
   if (count === 0) {
     id.innerHTML = "Time's Up";
   }
-}, 200);
+}, 1000)
+
+function incrementScore(num) {
+  score +=num
+  scoreText.innerText = score
+}
+
+startGame()
