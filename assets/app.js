574cb42664e66a6a3ca102aef40c9e2aadcf0110
jlw429 <jlw00329@gmail.com>
2020-12-27 21:33:54 -0500

    working quiz

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,15 @@
 const question = document.getElementById('question');
+const scoreText = document.getElementById('score');
 let choices = Array.from(document.querySelectorAll('.choice-text'));
-let count = 60;
+let count = 80;
 let score = 0;
 const score_points = 20;
 let acceptingAnswers = true;
 const max_questions = 5;
 let questionCounter = 0;
-let availableQuestions = []
-
+let availableQuestions = [];
+let nextQuestion = true;
+let lastQuestion = false;
 
 let questions = [
   {
@@ -19,7 +21,8 @@ let questions = [
     correct: 3,
   },
   {
-    question: 'This type of loop executes a block of code as long as a specified question is TRUE',
+    question:
+      'This type of loop executes a block of code as long as a specified question is TRUE',
     choice1: 'For Loop',
     choice2: 'While Loop',
     choice3: 'if statement',
@@ -49,7 +52,7 @@ let questions = [
     choice3: 'variable',
     choice4: 'Javascript',
     correct: 1,
-  }
+  },
 ];
 
 function startGame() {
@@ -57,51 +60,53 @@ function startGame() {
   score = 0;
   availableQuestions = [...questions];
   newQuestion();
-};
+}
+
+function handleOnClick(event) {
+  // if (!acceptingAnswers) return;
+  // acceptingAnswers = false;
+
+  let selectedChoice = event.target;
+  let selectedAnswer = selectedChoice.dataset['number'];
+
+  let classToApply =
+    selectedAnswer == currentQuestion.correct ? 'correct' : 'incorrect';
+
+  if (classToApply === 'correct') {
+    incrementScore(score_points);
+  }
+
+    nextQuestion = true;
+  if (availableQuestions.length == 0) {
+    lastQuestion = true;
+  }
+}
 
 function newQuestion() {
-  if(availableQuestions.length === 0 || questionCounter > max_questions) {
-    localStorage.setItem(score)
-  
-    return window.location.assign('/end.html')
-  }
-    questionCounter++
-    
-    let questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
+  nextQuestion = false;
+  questionCounter++;
+  let questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionsIndex];
 
-    choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
+  question.innerText = currentQuestion.question;
 
-    availableQuestions.splice(questionsIndex, 1);
-
-    acceptingAnswers = true
-
-    choices.forEach(function (choice) {
-    choice.addEventListener('click', event => {
-      if(!acceptingAnswers) return
-
-      acceptingAnswers = false;
-      let selectedChoice = event.target;
-      let selectedAnswer = selectedChoice.dataset['number'];
-
-      let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-
-      if(classToApply === 'correct') {;
-          incrementScore(score_points);
-      }
-
-      selectedChoice.parentElement.classList.add(classToApply);
-
-      
-        newQuestion();
-    
-    });
+ 
+  // acceptingAnswers = true;
+  choices.forEach(function (choice) {
+    const number = choice.dataset['number'];
+    choice.innerText = currentQuestion['choice' + number];
+    choice.removeEventListener('click', handleOnClick);
+    choice.addEventListener('click', handleOnClick);
   });
-};
+
+  availableQuestions.splice(questionsIndex, 1);
+}
+
+function endGame() {
+  localStorage.setItem('score', score);
+  window.location.replace('end.html');
+  //link to end page
+}
 
 //Timer
 setInterval(function () {
@@ -110,14 +115,22 @@ setInterval(function () {
     id = document.getElementById('countdown');
     id.innerHTML = count;
   }
+    if (lastQuestion) {
+    endGame();
+    return;
+  }
+  if (nextQuestion) {
+    newQuestion();
+  }
   if (count === 0) {
     id.innerHTML = "Time's Up";
+    endGame();
   }
-}, 1000)
+}, 1000);
 
 function incrementScore(num) {
-  score +=num
-  scoreText.innerText = score
+  score += num;
+  scoreText.innerText = score;
 }
 
-startGame()
+startGame();
